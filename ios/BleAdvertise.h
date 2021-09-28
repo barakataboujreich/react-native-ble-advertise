8338b53de2914771869b4dc91c59221ec5b5e98d
barakataboujreich <bako.j.93@gmail.com>
2021-09-28 15:38:45 +0300

    Update BleAdvertise.h

--- ios/BleAdvertise.h
+++ ios/BleAdvertise.h
@@ -2,10 +2,14 @@
 #import <React/RCTEventEmitter.h>
 #import <React/RCTLog.h>
 #import <CoreBluetooth/CoreBluetooth.h>
+@import CoreLocation;
 
-@interface BleAdvertise : RCTEventEmitter <RCTBridgeModule, CBPeripheralManagerDelegate, CBPeripheralDelegate>{
+@interface BleAdvertise : RCTEventEmitter <RCTBridgeModule, CBCentralManagerDelegate, CBPeripheralManagerDelegate, CBPeripheralDelegate> {
+    CBCentralManager *centralManager;
     CBPeripheralManager *peripheralManager;
     CLBeaconRegion *beaconRegion;
 }
+
+
 
 @end
