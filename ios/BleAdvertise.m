2b535723e5943e44e1a7f74e1b374ea10e3f4a02
barakataboujreich <bako.j.93@gmail.com>
2021-09-28 15:38:26 +0300

    Update BleAdvertise.m

--- ios/BleAdvertise.m
+++ ios/BleAdvertise.m
@@ -1,28 +1,29 @@
 #import "BleAdvertise.h"
 @import CoreBluetooth;
+@import CoreLocation;
 
 @implementation BleAdvertise
 
-RCT_EXPORT_MODULE(BleAdvertise)
+RCT_EXPORT_MODULE()
 
 RCT_EXPORT_METHOD(setCompanyId: (nonnull NSNumber *)companyId){
     RCTLogInfo(@"setCompanyId function called %@", companyId);
     self->peripheralManager = [[CBPeripheralManager alloc] initWithDelegate:self queue:nil options:nil];
 }
 
-RCT_EXPORT_METHOD(broadcast:(NSString *)uid major:(int)major minor:(int)minor resolve: (RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXPORT_METHOD(broadcast:(NSString *)uid major:(NSNumber *)major minor:(NSNumber *)minor resolve: (RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 {
     RCTLogInfo(@"Broadcast function called %@ with major %@ and minor %@", uid, major, minor);
     NSString *identifier = @"Kindoo";
 
     NSUUID *proximityUUID = [[NSUUID alloc] initWithUUIDString:uid];
-    self.beaconRegion = [[CLBeaconRegion alloc] initWithProximityUUID:proximityUUID major:major minor:minor identifier:identifier];
-    self.beaconRegion.notifyEntryStateOnDisplay = YES;
+    beaconRegion = [[CLBeaconRegion alloc] initWithProximityUUID:proximityUUID major:major minor:minor identifier:identifier];
+    beaconRegion.notifyEntryStateOnDisplay = YES;
 
-    if (!self.peripheralManager)
-      self.peripheralManager = [[CBPeripheralManager alloc] initWithDelegate:self queue:nil options:nil];
+    if (!peripheralManager)
+      peripheralManager = [[CBPeripheralManager alloc] initWithDelegate:self queue:nil options:nil];
 
-    if (self.peripheralManager.state != CBPeripheralManagerStatePoweredOn) {
+    if (peripheralManager.state != CBPeripheralManagerStatePoweredOn) {
         RCTLogInfo(@"Peripheral manager is off.");
         return;
     }
@@ -30,18 +31,15 @@ RCT_EXPORT_METHOD(broadcast:(NSString *)uid major:(int)major minor:(int)minor re
     time_t t;
     srand((unsigned) time(&t));
     
-    UInt16 major = [self.beaconRegion.major unsignedShortValue];
-    UInt16 minor = [self.beaconRegion.minor unsignedShortValue];
-    
-    CLBeaconRegion *region = [[CLBeaconRegion alloc] initWithProximityUUID:self.beaconRegion.proximityUUID major: major minor: minor identifier:self.beaconRegion.identifier];
+    CLBeaconRegion *region = [[CLBeaconRegion alloc] initWithProximityUUID:beaconRegion.proximityUUID major:[major doubleValue] minor:[minor doubleValue] identifier:beaconRegion.identifier];
     NSDictionary *beaconPeripheralData = [region peripheralDataWithMeasuredPower:nil];
-    [self.peripheralManager startAdvertising:beaconPeripheralData];
+    [peripheralManager startAdvertising:beaconPeripheralData];
 
     resolve(@"Broadcasting");
 }
 
 RCT_EXPORT_METHOD(stopBroadcast:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject){
-    [self.peripheralManager stopAdvertising];
+    [peripheralManager stopAdvertising];
     resolve(@"Stopping Broadcast");
 }
 
